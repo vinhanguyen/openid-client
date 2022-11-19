b0b6518899fd0b132b8f1886390dd22855cbc514
vinhanguyen <vinh.a.nguyen@gmail.com>
2022-11-18 22:25:23 -0500

    Make token instance variable in auth service

--- src/app/profile/profile.service.ts
+++ src/app/profile/profile.service.ts
@@ -1,24 +1,36 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { mergeMap } from 'rxjs';
+import { mergeMap, Subscription } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { selectToken } from '../auth/auth.selectors';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProfileService {
+export class ProfileService implements OnDestroy {
+  token!: string;
+  tokenSub!: Subscription;
 
   getUser() {
-    return this.store.select(selectToken).pipe(
-      mergeMap(token => this.http.get<any>(`${environment.api_endpoint}/user`, {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      }))
-    );
+    return this.http.get<any>(`${environment.api_endpoint}/user`, {
+      headers: {
+        Authorization: `Bearer ${this.token}`
+      }
+    });
   }
 
-  constructor(private http: HttpClient, private store: Store) { }
+  constructor(private http: HttpClient, private store: Store) {
+    this.tokenSub = this.store.select(selectToken).subscribe(token => {
+      if (token) {
+        this.token = token;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.tokenSub) {
+      this.tokenSub.unsubscribe();
+    }
+  }
 }
